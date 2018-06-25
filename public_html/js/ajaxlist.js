6682b04d097613c7b7e689a9510bb6b80e09f8b8
Tuyen Dang <tuyendq@gmail.com>
2018-06-25 22:23:13 +0700

    add country.json, add button loading country list

--- public_html/js/ajaxlist.js
+++ public_html/js/ajaxlist.js
@@ -1,4 +1,22 @@
-use strict
 function loadList(){
-
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","/data/country.json",true);
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4 && xhr.status == 200){
+            var list = document.getElementById("listcountry");
+            var data = xhr.responseText;
+            var vec = JSON.parse(data);
+            vec.foreach(function(ob){
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                var text = document.createTextNode(ob.country);
+                a.appendChild(text);
+                a.setAttribute("href","#");
+                li.appendChild(a);
+                list.appendChild(li);
+            });
+            $('#listcountry').listview('refresh');
+        }
+    }
+    xhr.send();
 }
